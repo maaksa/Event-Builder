cc67a4c43a9d94b7304ba89ac0fb080aed1152a0
Maksa <milosmilos@ymail.com>
2021-06-17 00:11:08 +0200

    created activity for adding event

---
+++ app/src/main/java/rs/raf/projekatjun/milos_maksimovic_rn4318/AddEventActivity.java
@@ -0,0 +1,14 @@
+package rs.raf.projekatjun.milos_maksimovic_rn4318;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_event);
+    }
+}
--- app/src/main/java/rs/raf/projekatjun/milos_maksimovic_rn4318/MainActivity.java
+++ app/src/main/java/rs/raf/projekatjun/milos_maksimovic_rn4318/MainActivity.java
@@ -1,13 +1,38 @@
 package rs.raf.projekatjun.milos_maksimovic_rn4318;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnAddEvent;
+    private Button btnShowEvents;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+    private void init() {
+        initView();
+        initListeners();
+    }
+
+    private void initView() {
+        btnAddEvent = findViewById(R.id.btnAddEventMainActivity);
+        btnShowEvents = findViewById(R.id.btnShowEventsMainActivity);
+    }
+
+    private void initListeners() {
+        btnAddEvent.setOnClickListener(v -> {
+            Intent intent = new Intent(this, AddEventActivity.class);
+            startActivity(intent);
+        });
     }
 }
